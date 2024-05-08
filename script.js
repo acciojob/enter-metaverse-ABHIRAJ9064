04ab1f9649e4ab9a9423c62066e825c7cf9a6074
ABHIRAJ9064 <159516528+ABHIRAJ9064@users.noreply.github.com>
2024-05-08 19:44:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let button1 = document.getElementId("enterBtn");
+let s = document.getEl;ementId("status");
+button1.addEventListener("click",handleClick);
+function handleClick(e) {
+	e.preventDefault();
+	let h1 = document.createElement("h1");
+	s.innerText=null;
+	h1.innerText="Entered Metaverse";
+	s.append(h1);
+	
+}
